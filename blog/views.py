95ebec6f41299b388faf5bb9b0e0367d48ea75e5
Winston Kinoti <prog.kinoti@gmail.com>
2022-04-10 20:52:10 +0300

    Created base template and inherited from it

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 10, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 11, 2022'
+    }
+]
+
+
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        'posts': posts,
+        'title': 'Home',
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {'title': 'About'})
