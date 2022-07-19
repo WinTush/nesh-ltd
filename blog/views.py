5cd7fb2b3bae364d7f684563a4b1bf56d818b9ce
Winston Kinoti <prog.kinoti@gmail.com>
2022-07-19 16:15:09 +0300

    Use Post class as the data source for views.home

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,15 @@
 from django.shortcuts import render
 
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 10, 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 11, 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
-        'title': 'Home',
+        "posts": Post.objects.all(),
+        "title": "Home",
     }
     return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html", {'title': 'About'})
+    return render(request, "blog/about.html", {"title": "About"})
