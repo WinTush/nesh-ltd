9d9ae9b179c4cfe110e734618a3f7391191240ea
Winston Kinoti <prog.kinoti@gmail.com>
2022-07-19 16:32:50 +0300

    Used black to lint.

--- nesh/asgi.py
+++ nesh/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nesh.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nesh.settings")
 
 application = get_asgi_application()
